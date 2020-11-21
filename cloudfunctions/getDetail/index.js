fcdde59d7c4a489089e1ed92f959f6bc3933cb25
shiql <460960620@qq.com>
2020-11-21 20:36:58 +0800

    完成提交

---
+++ cloudfunctions/getDetail/index.js
@@ -0,0 +1,10 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init()
+var rp = require('request-promise');
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+  return  rp(`https://frodo.douban.com/api/v2/movie/${event.movieid}?apiKey=054022eaeae0b00e0fc068c0c0a2102a`)
+}
---
+++ cloudfunctions/movielist/index.js
@@ -0,0 +1,11 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init()
+var rp = require('request-promise');
+// 云函数入口函数
+exports.main = async (event, context) => {
+
+  return  rp(`https://frodo.douban.com/api/v2/subject_collection/movie_showing/items?start=${event.start}&count=${event.count}
+             &apiKey=054022eaeae0b00e0fc068c0c0a2102a`)
+}
